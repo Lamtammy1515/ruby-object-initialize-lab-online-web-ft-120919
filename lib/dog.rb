6aa155694ed7abec8f360fd09bb06ff0e78758f0
Tammy Lam <lamtammy1515@gmail.com>
2019-12-16 21:16:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,24 @@ class Dog
   def initialize(breed)
     @breed = breed 
   end 
+  
+def name=(name)
+  @name = name 
 end 
+
+def name 
+  @name 
+end 
+
+def breed=(breed)
+  @breed = breed
+end 
+
+def breed 
+  @breed 
+end 
+end 
+
+fido = Dog.new
+fido.name = "Fido"
+fido.breed = "Mutt"
